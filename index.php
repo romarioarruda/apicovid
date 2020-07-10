0622651805f036093d5aed33aac90aecf8c1ffc8
Romário Arruda <35845386+romarioarruda@users.noreply.github.com>
2020-07-09 21:25:00 -0300

    Update index.php
    Update

--- index.php
+++ index.php
@@ -3,7 +3,6 @@ require_once 'vendor/autoload.php';
 require_once 'src/config/config.php';
 
 $dadosCovid = new dadosCovidController;
-$varreduraCovid = new execVarreduraCovidController;
 $newsCovid  = new ultimasNoticiasController;
 
 //Endpoints que pegam dados
@@ -13,8 +12,6 @@ Flight::route('GET /dados/recuperados', array($dadosCovid, 'getRecuperados'));
 Flight::route('GET /dados/obitos', array($dadosCovid, 'getObitos'));
 
 //Endpoints de execução de varredura
-Flight::route('GET /dados/atualizar-casos', array($varreduraCovid, 'execVarreduraApiGoverno'));
-Flight::route('GET /dados/atualizar-acumulados', array($varreduraCovid, 'execTotalAcumulado'));
 Flight::route('GET /dados/ultimas-noticias', array($newsCovid, 'execUltimasNoticias'));
 
 //Mapeando rota que não tem nada.
